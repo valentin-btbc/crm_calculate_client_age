42ceac8219fb680be6ac33ac0d563c139eb8e768
Valentin BELAUD <valentin@noosys.fr>
2018-06-04 11:02:16 +0200

    def _calculate_age down

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','crm','web_studio'],
 
     # always loaded
     'data': [
--- models/models.py
+++ models/models.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import datetime, timedelta
+import logging
+_logger = logging.getLogger(__name__)
 
-# class crm_calculate_client_age(models.Model):
-#     _name = 'crm_calculate_client_age.crm_calculate_client_age'
+class crm_calculate_client_age(models.Model):
+    _inherit = 'res.partner'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    age = fields.Integer(compute="_calculate_age")
+    # age = fields.Integer()
+
+    @api.depends('x_studio_field_0yAlw')
+    def _calculate_age(self):
+        for record in self:
+            if record.x_studio_field_0yAlw:
+                born = datetime.strptime(record.x_studio_field_0yAlw, '%Y-%m-%d %H:%M:%S')
+                _logger.info('---------------------- VAR : born --------------------------------')
+                _logger.info(born)
+                date_today = datetime.now()
+
+                record.age = date_today.year - born.year
